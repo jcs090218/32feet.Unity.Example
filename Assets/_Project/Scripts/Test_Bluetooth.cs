5e3499644cff38aec1aeb22b6b01bbbcecb84c66
JenChieh <jcs090218@gmail.com>
2021-03-19 17:10:49 +0800

    Add test scripts

---
+++ Assets/_Project/Scripts/Test_Bluetooth.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * $File: Test_Bluetooth.cs $
+ * $Date: #CREATIONDATE# $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information 
+ *	                 Copyright © #CREATEYEAR# by Shen, Jen-Chieh $
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Test for 32feet library.
+/// </summary>
+public class Test_Bluetooth 
+    : MonoBehaviour 
+{
+    /* Variables */
+
+    /* Setter & Getter */
+
+    /* Functions */
+
+}
